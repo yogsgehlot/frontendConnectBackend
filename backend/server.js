aa54e16797fa355ac1d33d72c0c89268a4c5c13f
Yogesh Gehlot <75558691+yogsgehlot@users.noreply.github.com>
2023-12-31 15:24:29 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+import express from "express";
+
+const app = express();
+
+// app.get('/', (req,res) =>{
+//     res.send('Server is ready');
+// });
+
+app.get("/api/records", (req, res) => {
+  const records = [
+    {
+      UID: 4454,
+      Name: "Yogesh",
+      Address: "Rajasthan",
+    },
+    {
+      UID: 4465,
+      Name: "Harsh",
+      Address: "Delhi",
+    },
+    {
+      UID: 4424,
+      Name: "Ayush",
+      Address: "MadhyaPradesh",
+    },
+    {
+      UID: 4430,
+      Name: "Bhoma",
+      Address: "Rajasthan",
+    },
+  ];
+
+  res.send(records);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
